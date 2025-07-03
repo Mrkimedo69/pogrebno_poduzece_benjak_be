87b534dba6aadce470ff8a2ab9d3d7841b5db934
mrkimedo69 <luka.mavric26051999@gmail.com>
2025-07-03 15:47:41 +0200

    Added api, db space for stone materials

--- src/stone-materials/stone-materials.controller.ts
+++ src/stone-materials/stone-materials.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { StoneMaterialsService } from './stone-materials.service';
+import { StoneMaterial } from './stone-materials.entity';
 
 @Controller('stone-materials')
-export class StoneMaterialsController {}
+export class StoneMaterialsController {
+  constructor(private readonly service: StoneMaterialsService) {}
+
+  @Get()
+  findAll(): Promise<StoneMaterial[]> {
+    return this.service.findAll();
+  }
+
+  @Post()
+  create(@Body() data: Partial<StoneMaterial>): Promise<StoneMaterial> {
+    return this.service.create(data);
+  }
+}

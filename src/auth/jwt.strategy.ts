76162332886ff3a2cf47183804fbc1bbf3f747bb
mrkimedo69 <luka.mavric26051999@gmail.com>
2025-07-11 17:03:10 +0200

    Quick fix

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: any) {
     return {
-      userId: payload.sub,
+      id: payload.sub,
       email: payload.email,
       fullName: payload.fullName,
       role: payload.role

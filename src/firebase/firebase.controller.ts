f5b45203591eecf1b0fa3ac3a5bdcaeadcf00a7c
mrkimedo69 <luka.mavric26051999@gmail.com>
2025-07-25 23:15:55 +0200

    Prod quick fix

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -10,6 +10,7 @@ import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { Express } from 'express';
 import { existsSync, mkdirSync } from 'fs';
+import { memoryStorage } from 'multer';
 
 @Controller('upload')
 export class UploadController {
@@ -17,23 +18,28 @@ export class UploadController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
-      destination: (req, file, cb) => {
-        const dir = './uploads';
-        if (!existsSync(dir)) mkdirSync(dir);
-        cb(null, dir);
-      },
-      filename: (req, file, cb) => {
-        const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, `${uniqueName}${extname(file.originalname)}`);
-      }
+    storage: process.env.NODE_ENV === 'production'
+      ? memoryStorage()
+      : diskStorage({
+        destination: (req, file, cb) => {
+          const dir = './uploads';
+          if (!existsSync(dir)) mkdirSync(dir);
+          cb(null, dir);
+        },
+        filename: (req, file, cb) => {
+          const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, `${uniqueName}${extname(file.originalname)}`);
+        }
     }),
   }))
   async upload(@UploadedFile() file: Express.Multer.File) {
     if (process.env.NODE_ENV === 'production') {
+      if (!file || !file.buffer) {
+        throw new Error('Firebase upload failed: file buffer missing.');
+      }
       const url = await this.firebaseService.uploadImage(file);
       return { imageUrl: url };
-    } else {
+    }else {
       const localUrl = `/uploads/${file.filename}`;
       return { imageUrl: localUrl };
     }

49c9468d2505e7f5ac8a951e7a8d568c01cce891
mrkimedo69 <luka.mavric26051999@gmail.com>
2025-08-01 20:51:00 +0200

    CRUD for admin materials

--- src/stone-materials/stone-materials.controller.ts
+++ src/stone-materials/stone-materials.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UseGuards
+} from '@nestjs/common';
 import { StoneMaterialsService } from './stone-materials.service';
 import { StoneMaterial } from './stone-materials.entity';
 import { CreateStoneMaterialDto } from './dto/stone-materials.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('stone-materials')
 export class StoneMaterialsController {
   constructor(private readonly service: StoneMaterialsService) {}
@@ -11,9 +24,32 @@ export class StoneMaterialsController {
   findAll(): Promise<StoneMaterial[]> {
     return this.service.findAll();
   }
-  
+
+  @Roles('admin')
+  @Get('admin')
+  findAllAdmin(): Promise<StoneMaterial[]> {
+    return this.service.findAll();
+  }
+
+  @Roles('admin')
   @Post()
   create(@Body() dto: CreateStoneMaterialDto): Promise<StoneMaterial> {
     return this.service.create(dto);
   }
+
+  @Roles('admin')
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() data: Partial<StoneMaterial>,
+  ): Promise<StoneMaterial> {
+    return this.service.update(id, data);
+  }
+
+  @Roles('admin')
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.service.remove(id);
+  }
+
 }
